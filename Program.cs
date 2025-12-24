a0a82eec12d8b2eff59b5c089a21e32902a7c089
milos pusic <milos.pusic@prvabeogim.edu.rs>
2025-12-24 11:07:23 +0100

    sifarnik 1

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace dnevnik410a
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             // Application.Run(new Login());
-            Application.Run(new raspodela());
+            Application.Run(new sifarnik());
         }
     }
 }
